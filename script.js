cace82ddef8731f8afd90074e97a0ad3a3820ceb
Gopal Kumar Keshri <gopalkumarkeshri549@gmail.com>
2025-02-05 19:27:54 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//Menu
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+    navbar.classList.toggle("open-menu");
+}
+window.onscroll = () => {
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+}
+
+// scrollreveal
+const animate = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: "2500",
+    delay: "400",
+});
+
+animate.reveal(".home-text", { origin: "left" });
+animate.reveal(".home-img", { origin: "bottom" });
+animate.reveal(".heading , .newsletter h2", { origin: "top" });
+animate.reveal("header,.feature-box, .feature-menu-box,.item-box,.m-item-box,.t-box,.newsletter", { Interval: 100 });
